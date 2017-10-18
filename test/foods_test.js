e41eedde6dc27d561035c266d676d4d5ee3a1402
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-18 11:19:40 -0600

    passing happy and sad tests for food_test.js

--- test/foods_test.js
+++ test/foods_test.js
@@ -7,4 +7,42 @@ const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 const foods = require('../lib/models/foods')
 
-eval(pry.it)
+describe('Foods', function(){
+  before(function(done){
+    this.port = 9876
+    this.server = app.listen(this.port, function(err, result){
+      if(err){return done(err)}
+      done()
+    })
+    this.request = request.defaults({
+      baseURL: 'http://localhost:9876'
+    })
+  })
+  after(function(){
+    this.server.close()
+  })
+
+  describe('GET /api/v1/foods/:id', function(){
+    it('should exist', function(){
+      assert(app)
+    })
+    it('should return a 404 for an unregistered id', function(done){
+      var id = 100;
+      this.request.get('http://localhost:9876/api/v1/foods/' + id, function(error, response){
+        if(error){done(error)}
+        assert.equal(response.statusCode, 404)
+        done()
+      })
+    })
+    it('should return a json object of a food with registered id', function(done){
+      var id = 1;
+      var food1 = {id: 1, name: "Banana", calories: 105};
+      this.request.get('http://localhost:9876/api/v1/foods/' + id, function(error, response){
+        if(error){done(error)}
+        let parsedFood = JSON.parse(response.body)
+        assert.deepEqual(parsedFood, food1)
+        done()
+      })
+    })
+  });
+});
