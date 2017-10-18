3215e61f0a363f9e596dc5e9113f777f37987a36
Katie Keel <akatiekeel@gmail.com>
2017-10-18 10:06:37 -0600

    Got npm test to quit hanging with --exit in script

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require('express')
 var app = express()
 
-// app.set('port', process.env.PORT || 9876)
-// app.locals.title = 'Secret Box'
+app.set('port', process.env.PORT || 9876)
+app.locals.title = 'Secret Box'
 
 app.get('/', function(request, response) {
   response.send('It\'s a secret to everyone.')
 })
 
-// if (!module.parent) {
-//   app.listen(app.get('port'), function() {
-//     console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-//   });
-// }
+if (!module.parent) {
+  app.listen(app.get('port'), function() {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
 
 module.exports = app
