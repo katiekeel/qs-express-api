df7c9efecfdff347793541abde98ef58ac035e36
Katie Keel <akatiekeel@gmail.com>
2017-10-19 16:47:24 -0600

    Get all meals model method tested

--- test/meal_test.js
+++ test/meal_test.js
@@ -3,4 +3,16 @@ const pry = require('pryjs');
 const environment = process.env.NODE_ENV || 'test'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
-const Food = require('../lib/models/food')
+const Meal = require('../lib/models/meal')
+
+describe('Meal model', function () {
+  it('gets all meals from the database', function() {
+    Meal.getAll()
+    .then(function(meals) {
+      assert.include(meals.rows[0].name, "Breakfast")
+      assert.include(meals.rows[1].name, "Dinner")
+      assert.include(meals.rows[2].name, "Snack")
+      assert.include(meals.rows[3].name, "Lunch")
+    })
+  })
+})
