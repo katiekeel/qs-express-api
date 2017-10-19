3126adc95d5ea079b2c711b80d75c1f47651ca71
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-19 14:46:27 -0600

    mends foods_test.js to work with or without seeded data

--- test/foods_test.js
+++ test/foods_test.js
@@ -7,7 +7,7 @@ const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 const foods = require('../lib/models/food')
 
-xdescribe('Foods', function(){
+describe('Foods', function(){
   before(function(done){
     this.port = 3000
     this.server = app.listen(this.port, function(err, result){
@@ -16,6 +16,21 @@ xdescribe('Foods', function(){
     })
     this.request = request.defaults({
       baseUrl: 'http://localhost:9876'
+    })
+  })
+  var formerId;
+  beforeEach((done) => {
+    var food = {
+      name: 'Carrot Cake',
+      calories: '450'
+    }
+    foods.create(food)
+    .then(() => {
+      database.raw("SELECT id FROM foods WHERE name = 'Carrot Cake';")
+      .then((data) => {
+        formerId = data.rows[0].id
+        done()
+      })
     })
   })
   after(function(){
@@ -27,7 +42,7 @@ xdescribe('Foods', function(){
       assert(app)
     })
     it('should return a 404 for an unregistered id', function(done){
-      let id = 1000;
+      let id = 10000;
       this.request.get('/api/v1/foods/' + id, function(error, response){
         if(error){done(error)}
         assert.equal(response.statusCode, 404)
@@ -35,18 +50,21 @@ xdescribe('Foods', function(){
       })
     })
     it('should return a json object of a food with registered id', function(done){
-      let id = 1;
-      var food1 = {id: 1, name: "Banana", calories: 105};
-      this.request.get('/api/v1/foods/' + id, function(error, response){
+      var food1 = {id: formerId, name: "Carrot Cake", calories: 450};
+      this.request.get('/api/v1/foods/' + formerId, function(error, response){
         if(error){done(error)}
         let parsedFood = JSON.parse(response.body)
         assert.deepEqual(parsedFood, food1)
-        done()
       })
+      done()
     })
   });
   describe('POST /api/v1/foods', function(){
     beforeEach((done) => {
+      database.raw("DELETE FROM foods WHERE name = 'Carrot Cake';")
+      .then(() => done())
+    })
+    afterEach((done) => {
       database.raw("DELETE FROM foods WHERE name = 'Carrot Cake';")
       .then(() => done())
     })
@@ -63,8 +81,8 @@ xdescribe('Foods', function(){
         assert.equal('Carrot Cake', parsedFood.name)
         assert.equal(450, parsedFood.calories)
         assert.equal(response.statusCode, 201)
-        done()
       })
+      done()
     })
   });
   describe('PATCH', function(){
@@ -97,20 +115,12 @@ xdescribe('Foods', function(){
           assert.hasAllDeepKeys(patchedResponse, ['id', 'name', 'calories'])
           assert.deepEqual(patchedResponse, food)
           assert.isTrue(patchedResponse.name.includes('CreamCheese'))
-          done()
         })
+        done()
       })
     })
   })
   describe('DELETE', function(){
-    beforeEach((done) => {
-      var food = {
-          name: 'Carrot Cake',
-          calories: '450'
-        }
-      foods.create(food)
-      .then(() => done())
-    })
     it('Deletes a food item from the db given an id', function(done){
       database.raw('SELECT MAX(id) FROM foods;')
         .then((data) => {
