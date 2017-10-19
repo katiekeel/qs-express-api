2930d68a6481f75be7d267f6cb61d2d0b7ad2eae
Katie Keel <akatiekeel@gmail.com>
2017-10-19 16:53:06 -0600

    Get one meal model method written and tested

--- test/meal_test.js
+++ test/meal_test.js
@@ -6,13 +6,24 @@ const database = require('knex')(configuration)
 const Meal = require('../lib/models/meal')
 
 describe('Meal model', function () {
-  it('gets all meals from the database', function() {
-    Meal.getAll()
-    .then(function(meals) {
-      assert.include(meals.rows[0].name, "Breakfast")
-      assert.include(meals.rows[1].name, "Dinner")
-      assert.include(meals.rows[2].name, "Snack")
-      assert.include(meals.rows[3].name, "Lunch")
+  describe('getAll', function () {
+    it('gets all meals from the database', function() {
+      Meal.getAll()
+      .then(function(meals) {
+        assert.include(meals.rows[0].name, "Breakfast")
+        assert.include(meals.rows[1].name, "Dinner")
+        assert.include(meals.rows[2].name, "Snack")
+        assert.include(meals.rows[3].name, "Lunch")
+      })
+    })
+  })
+
+  describe('find', function() {
+    it('gets one meal from the database', function() {
+      Meal.find(1)
+      .then(function(meals) {
+        assert.include(meals.rows[0].name, "Breakfast")
+      })
     })
   })
 })
