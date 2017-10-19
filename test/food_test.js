da1db093fd0fa9764ff2c189e62bbf66ef2ce069
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-10-19 11:46:26 -0600

    organizes test_file

--- test/food_test.js
+++ test/food_test.js
@@ -4,100 +4,100 @@ var environment = process.env.NODE_ENV || 'test'
 var configuration = require('../knexfile')[environment]
 var database = require('knex')(configuration)
 const Food = require('../lib/models/food')
-
-describe('Food Model -- Find', function(){
-  it('can find an existing food given id', function(){
-    var foodItem;
-    var id = 1
-    var food1 = {id: 1, name: 'Banana', calories: 105}
-    Food.find(id)
+describe('Food Model -- modular functions', function(){
+  describe('-- Find', function(){
+    it('can find an existing food given id', function(){
+      var foodItem;
+      var id = 1
+      var food1 = {id: 1, name: 'Banana', calories: 105}
+      Food.find(id)
       .then(function(food){
         foodItem = food.rows[0]
         assert.deepEqual(foodItem, food1)
       })
-  })
-});
-describe('Food Model -- Create', function(){
-  afterEach((done) => {
-    database.raw("DELETE FROM foods WHERE name = 'Carrot Cake';")
-    .then(() => done())
-  })
-  it('can create a new food item', function(done){
-    database.raw('SELECT MAX(id) from foods;')
+    })
+  });
+  describe('-- Create', function(){
+    afterEach((done) => {
+      database.raw("DELETE FROM foods WHERE name = 'Carrot Cake';")
+      .then(() => done())
+    })
+    it('can create a new food item', function(done){
+      database.raw('SELECT MAX(id) from foods;')
       .then((data) => {
         var id = data.rows[0].max;
         var newId = id + 3
         var newFood = {id: newId, name: 'Carrot Cake', calories: 299};
         Food.create(newFood)
-          .then(function(food){
-            let returnFood = food.rows[0]
-            assert.deepEqual(returnFood, newFood)
-            done()
-          })
+        .then(function(food){
+          let returnFood = food.rows[0]
+          assert.deepEqual(returnFood, newFood)
+          done()
+        })
       })
-  })
-});
-describe('Food Model -- Update', function(){
-  var formerId;
-  beforeEach((done) => {
-    var food = {
+    })
+  });
+  describe('-- Update', function(){
+    var formerId;
+    beforeEach((done) => {
+      var food = {
         name: 'Carrot Cake',
         calories: '450'
       }
-    Food.create(food)
-    .then(() => {
-      database.raw("SELECT id FROM foods WHERE name = 'Carrot Cake';")
+      Food.create(food)
+      .then(() => {
+        database.raw("SELECT id FROM foods WHERE name = 'Carrot Cake';")
         .then((data) => {
           formerId = data.rows[0].id
           done()
         })
+      })
     })
-  })
-  afterEach((done) => {
-    database.raw("SELECT id FROM foods WHERE name ='Coffee Cake';")
+    afterEach((done) => {
+      database.raw("SELECT id FROM foods WHERE name ='Coffee Cake';")
       .then((data) => {
         var editedFoodId = data.rows[0].id;
         var previousDeets = {name: 'Carrot Cake', calories: 450};
         Food.update(editedFoodId, previousDeets)
-          .then(() => done())
+        .then(() => done())
       })
-  })
-  it('can edit and update a food with correct properties', function(done){
-    database.raw(`SELECT id, name, calories FROM foods WHERE id=${formerId};`)
+    })
+    it('can edit and update a food with correct properties', function(done){
+      database.raw(`SELECT id, name, calories FROM foods WHERE id=${formerId};`)
       .then((data) => {
         var previousFoodDetails = data.rows[0];
         assert.equal(previousFoodDetails.name, 'Carrot Cake')
         assert.equal(previousFoodDetails.calories, 450)
         formerId = previousFoodDetails.id;
         Food.update(formerId, {name: 'Coffee Cake', calories: 199})
-          .then((data) => {
-            let updatedFood = {id: formerId, name: 'Coffee Cake', calories: 199}
-            let editedFood = data.rows[0];
-            assert.equal(editedFood.id, formerId)
-            assert.deepEqual(editedFood, updatedFood)
-          })
-          done()
+        .then((data) => {
+          let updatedFood = {id: formerId, name: 'Coffee Cake', calories: 199}
+          let editedFood = data.rows[0];
+          assert.equal(editedFood.id, formerId)
+          assert.deepEqual(editedFood, updatedFood)
+        })
+        done()
       })
-  })
-});
-describe('Food Model -- Destroy', function(){
-  var cakeId;
-  beforeEach((done) => {
-    var food = {
+    })
+  });
+  describe('-- Destroy', function(){
+    var cakeId;
+    beforeEach((done) => {
+      var food = {
         name: 'Chocolate Cake',
         calories: '450'
       }
-    Food.create(food)
-    .then(() => {
-      database.raw("SELECT id FROM foods WHERE name = 'Chocolate Cake';")
+      Food.create(food)
+      .then(() => {
+        database.raw("SELECT id FROM foods WHERE name = 'Chocolate Cake';")
         .then((data) => {
           cakeId = data.rows[0].id
           done()
         })
+      })
     })
-  })
-  it('can delete and returns an item based on id', function(done){
-    Food.destroy(cakeId)
+    it('can delete and returns an item based on id', function(done){
+      Food.destroy(cakeId)
       .then((data) => {
         var returnedFood = data.rows[0]
         var formerFood = {id: cakeId, name: 'Chocolate Cake', calories: 450}
@@ -105,10 +105,12 @@ describe('Food Model -- Destroy', function(){
       })
       .then(() => {
         database.raw("SELECT * FROM foods WHERE name = 'Chocolate Cake';")
-          .then((data)=> {
-            assert.isUndefined(data.rows[0])
-          })
+        .then((data)=> {
+          assert.isUndefined
+          (data.rows[0])
+        })
       })
       done()
+    })
   })
 })
