d4d663ae70910ae867a489b90e33051de3fae5fa
Katie Keel <akatiekeel@gmail.com>
2017-10-19 19:12:35 -0600

    maybe this cors thing will make it work

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE, OPTIONS")
+  next();
+});
 app.set('port', process.env.PORT || 9876)
 app.locals.title = 'QS Express Api'
 
