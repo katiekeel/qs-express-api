4ed2e6cac5c4cae446cc757d669ad546bf5ee546
Katie Keel <akatiekeel@gmail.com>
2017-10-23 14:19:04 -0600

    Moved CORS header portion to top of server file

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const Foods = require('./lib/controllers/foods')
 const Meals = require('./lib/controllers/meals')
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE, OPTIONS")
   next();
 });
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'QS Express Api'
 
