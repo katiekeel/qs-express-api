2d662e4206424f63a5b554edbeb347e6758a4da3
Katie Keel <akatiekeel@gmail.com>
2017-10-23 14:52:53 -0600

    Added a different port listener in app.use, and reintroduced CORS header

--- server.js
+++ server.js
@@ -6,17 +6,12 @@ const Meals = require('./lib/controllers/meals')
 const bodyParser = require('body-parser')
 const environment = process.env.NODE_ENV || 'development';
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE, OPTIONS")
-//   if (req.headers['x-forwarded-proto'] === 'https') {
-//       res.redirect('http://' + req.hostname + req.url);
-//     } else {
-//       next();
-//     }
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE, OPTIONS")
+  next();
+});
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -46,7 +41,7 @@ app.post('/api/v1/meals/:meal_id/foods/:food_id', Meals.postMealFood);
 app.delete('/api/v1/meals/:meal_id/foods/:food_id', Meals.deleteMealFood);
 
 if (!module.parent) {
-  app.listen(app.get('port'), function() {
+  app.listen(app.get(process.env.PORT || 3000), function() {
     console.log(`${app.locals.title} is running on ${app.get('port')}.`);
   });
 }
