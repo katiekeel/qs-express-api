9e835444254233141c0805acc543d43a2f0b9de8
Katie Keel <akatiekeel@gmail.com>
2017-10-23 14:37:48 -0600

    Added some more environment config

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const food = require('./lib/models/food')
 const Foods = require('./lib/controllers/foods')
 const Meals = require('./lib/controllers/meals')
 const bodyParser = require('body-parser')
+const environment = process.env.NODE_ENV || 'development';
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
