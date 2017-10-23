6de0bd68876be5e4b2f26a10e2421499e99c103a
Katie Keel <akatiekeel@gmail.com>
2017-10-23 15:04:36 -0600

    Removed typo in app.listen

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const Meals = require('./lib/controllers/meals')
 const bodyParser = require('body-parser')
 const environment = process.env.NODE_ENV || 'development';
 
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE, OPTIONS")
   next();
 });
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'QS Express Api'
 
@@ -41,9 +41,9 @@ app.post('/api/v1/meals/:meal_id/foods/:food_id', Meals.postMealFood);
 app.delete('/api/v1/meals/:meal_id/foods/:food_id', Meals.deleteMealFood);
 
 if (!module.parent) {
-  app.listen(app.get(process.env.PORT || 3000), function() {
-    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-  });
+  app.listen(process.env.PORT || 3000, function() {
+    console.log(`${app.locals.title} is running on port 3000.`)
+  })
 }
 
 module.exports = app
