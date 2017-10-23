d8822f4b9dd52a953cc511cfa0265b45bdf22fe1
Katie Keel <akatiekeel@gmail.com>
2017-10-23 14:25:41 -0600

    Added a potential fix for http/https issues

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, PATCH, GET, DELETE, OPTIONS")
+  if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+      next();
+    }
   next();
 });
 
